112072c5ed111970913b8274e2035c11b849650d
Peter Bom Jakobsen <pejak1990@gmail.com>
2021-09-02 15:36:55 +0200

    token: initial tokens added
    The minimal tokens added for the source language.
    A new type TokenType is defined to give more uniqueness - as the token
    type could just be a simple string type.

---
+++ token/token.go
@@ -0,0 +1,21 @@
+// Package token defines the tokens that makes up the source language for the
+// didactic compiler.
+package token
+
+type TokenType string
+
+const (
+	Illegal TokenType = "ILLEGAL"
+	Eof     TokenType = "EOF"
+
+	// Literals
+	Int TokenType = "INT" // 0123456789
+
+	// Keywords
+	Print TokenType = "PRINT"
+)
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}

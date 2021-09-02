44f9763783351de6d2d038992dd1ed8b9bea5452
Peter Bom Jakobsen <pejak1990@gmail.com>
2021-09-02 16:46:25 +0200

    lexer: basic lexer
    The lexer can lex the simple input "print 42".

---
+++ lexer/lexer.go
@@ -0,0 +1,110 @@
+// Package lexer implements a lexer (scanner) for the source language for the
+// didactic compiler.
+package lexer
+
+import (
+	"github.com/Glorforidor/didactic_compiler/token"
+)
+
+const eof byte = 0
+
+type Lexer struct {
+	input        string
+	position     int  // current position in input (points to current char)
+	readPosition int  // current reading position in input (after current char)
+	ch           byte // current char under examination
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+// readChar reads next character and advance positions accordingly.
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = eof
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	l.skipWhiteSpace()
+
+	var tok token.Token
+	switch l.ch {
+	case eof:
+		tok.Literal = ""
+		tok.Type = token.Eof
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.Print
+
+			// readIdentifier advances read positions, and therefore we must
+			// return early so we do not advance further.
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Literal = l.readNumber()
+			tok.Type = token.Int
+
+			// readNumber advances read positions, and therefore we must
+			// return early so we do not advance further.
+			return tok
+		} else {
+			tok = token.Token{Type: token.Illegal, Literal: string(l.ch)}
+		}
+	}
+
+	// Advance this pointers so l.ch is updated for the next invocation of
+	// NextToken.
+	l.readChar()
+
+	return tok
+}
+
+// isLetter check whether ch is a letter. It includes "_" as a letter.
+func isLetter(ch byte) bool {
+	// Checking whether ch is a letter is done by checking if the byte is
+	// within the ascii values of 'a'...'z' and 'A'...'Z'.
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+// isDigit check whether ch is a number.
+func isDigit(ch byte) bool {
+	// Checking whether ch is a number is done by checking if the byte is
+	// within the ascii values of '0'...'9'.
+	return '0' <= ch && ch <= '9'
+}
+
+// readIdentifier reads an identifier from l.input.
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) skipWhiteSpace() {
+	// keep advancing the input positions until hitting a non whitespace
+	// character
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
---
+++ lexer/lexer_test.go
@@ -0,0 +1,35 @@
+package lexer
+
+import (
+	"testing"
+
+	"github.com/Glorforidor/didactic_compiler/token"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `print 42`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.Print, "print"},
+		{token.Int, "42"},
+		{token.Eof, ""},
+	}
+
+	l := New(input)
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i,
+				tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i,
+				tt.expectedLiteral, tok.Literal)
+		}
+	}
+}

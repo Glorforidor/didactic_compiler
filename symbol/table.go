7eece8346f07ad8a6861937e5bc14379c47e71c3
Peter Bom Jakobsen <pejak1990@gmail.com>
2021-10-10 23:50:16 +0200

    symbol: scope and keep track of num of definitions.
    The symbol table holds an outer table, which creates the effect of
    scoping. If the outer table is nil, then it is the global scope.
    
    The number of definitions keeps track how many identifiers was declared
    in a given scope. This is used to know how much stack space needs to be
    reserved for a given block.
    
    On the symbols the Which attribute is used as part of the calculation of
    the offset on the stack.

--- symbol/table.go
+++ symbol/table.go
@@ -10,12 +10,14 @@ type SymbolScope int
 
 const (
 	GlobalScope SymbolScope = iota
+	LocalScope
 )
 
 type Symbol struct {
 	Name  string      // The identifier.
 	Type  types.Type  // The type of a symbol.
 	Scope SymbolScope // In which scope is the identifier.
+	Which int         // The ordinal position of variable (local or param)
 }
 
 // Code returns the assembly code for the symbol.
@@ -25,13 +27,18 @@ func (s *Symbol) Code() string {
 		// In the global scope it is always just the name of the symbol as it
 		// refers to a label.
 		return s.Name
+	case LocalScope:
+		return fmt.Sprintf("%d(sp)", s.Which*8)
 	default:
-		return ""
+		panic("Symbol did not have a scope!")
 	}
 }
 
 type Table struct {
+	Outer *Table
 	store map[string]Symbol
+
+	NumDefinitions int
 }
 
 func NewTable() *Table {
@@ -40,17 +47,36 @@ func NewTable() *Table {
 	}
 }
 
+func NewEnclosedTable(outer *Table) *Table {
+	s := NewTable()
+	s.Outer = outer
+	return s
+}
+
 func (st *Table) Define(name string, t types.Type) (Symbol, error) {
 	if s, ok := st.store[name]; ok {
-		return s, fmt.Errorf("identifier: %s already defined in scope", name)
+		// TODO: better error message - what scope? maybe just say the variable
+		// is already declared.
+		return s, fmt.Errorf("identifier: %q already defined in scope", name)
 	}
 
-	s := Symbol{Name: name, Scope: GlobalScope, Type: t}
+	s := Symbol{Name: name, Type: t, Which: st.NumDefinitions}
+	if st.Outer == nil {
+		s.Scope = GlobalScope
+	} else {
+		s.Scope = LocalScope
+	}
 	st.store[name] = s
+	st.NumDefinitions++
+
 	return s, nil
 }
 
 func (st *Table) Resolve(name string) (Symbol, bool) {
 	s, ok := st.store[name]
+	if !ok && st.Outer != nil {
+		s, ok := st.Outer.Resolve(name)
+		return s, ok
+	}
 	return s, ok
 }
--- symbol/table_test.go
+++ symbol/table_test.go
@@ -8,8 +8,10 @@ import (
 
 func TestDefine(t *testing.T) {
 	expected := map[string]Symbol{
-		"x": Symbol{Name: "x", Scope: GlobalScope, Type: types.Type{Kind: types.Int}},
-		"y": Symbol{Name: "y", Scope: GlobalScope, Type: types.Type{Kind: types.Int}},
+		"x": Symbol{Name: "x", Scope: GlobalScope, Type: types.Type{Kind: types.Int}, Which: 0},
+		"y": Symbol{Name: "y", Scope: GlobalScope, Type: types.Type{Kind: types.Int}, Which: 1},
+		"a": Symbol{Name: "a", Scope: LocalScope, Type: types.Type{Kind: types.Int}, Which: 0},
+		"b": Symbol{Name: "b", Scope: LocalScope, Type: types.Type{Kind: types.Int}, Which: 1},
 	}
 
 	global := NewTable()
@@ -23,6 +25,18 @@ func TestDefine(t *testing.T) {
 	if y != expected["y"] {
 		t.Errorf("expected y=%+v, got=%+v", expected["y"], y)
 	}
+
+	local := NewEnclosedTable(global)
+
+	a, _ := local.Define("a", types.Type{Kind: types.Int})
+	if a != expected["a"] {
+		t.Errorf("expected a=%+v, got=%+v", expected["a"], a)
+	}
+
+	b, _ := local.Define("b", types.Type{Kind: types.Int})
+	if b != expected["b"] {
+		t.Errorf("expected a=%+v, got=%+v", expected["b"], b)
+	}
 }
 
 func TestResolveGlobal(t *testing.T) {
@@ -31,8 +45,8 @@ func TestResolveGlobal(t *testing.T) {
 	global.Define("y", types.Type{Kind: types.Int})
 
 	expected := []Symbol{
-		Symbol{Name: "x", Scope: GlobalScope, Type: types.Type{Kind: types.Int}},
-		Symbol{Name: "y", Scope: GlobalScope, Type: types.Type{Kind: types.Int}},
+		Symbol{Name: "x", Scope: GlobalScope, Type: types.Type{Kind: types.Int}, Which: 0},
+		Symbol{Name: "y", Scope: GlobalScope, Type: types.Type{Kind: types.Int}, Which: 1},
 	}
 
 	for _, sym := range expected {
@@ -46,6 +60,35 @@ func TestResolveGlobal(t *testing.T) {
 				t.Fatalf("expected %s to resolve to %+v, got=%+v", sym.Name, sym, result)
 			}
 		})
+	}
+}
+
+func TestResolveLocal(t *testing.T) {
+	global := NewTable()
+	global.Define("a", types.Type{Kind: types.Int})
+	global.Define("b", types.Type{Kind: types.Int})
+
+	local := NewEnclosedTable(global)
+	local.Define("c", types.Type{Kind: types.Int})
+	local.Define("d", types.Type{Kind: types.Int})
+
+	expected := []Symbol{
+		Symbol{Name: "a", Scope: GlobalScope, Type: types.Type{Kind: types.Int}, Which: 0},
+		Symbol{Name: "b", Scope: GlobalScope, Type: types.Type{Kind: types.Int}, Which: 1},
+		Symbol{Name: "c", Scope: LocalScope, Type: types.Type{Kind: types.Int}, Which: 0},
+		Symbol{Name: "d", Scope: LocalScope, Type: types.Type{Kind: types.Int}, Which: 1},
+	}
+
+	for _, sym := range expected {
+		result, ok := local.Resolve(sym.Name)
+		if !ok {
+			t.Errorf("name %s not resolvable", sym.Name)
+			continue
+		}
+
+		if result != sym {
+			t.Errorf("expected %s to resolve to %+v, got=%+v", sym.Name, sym, result)
+		}
 	}
 }
 
